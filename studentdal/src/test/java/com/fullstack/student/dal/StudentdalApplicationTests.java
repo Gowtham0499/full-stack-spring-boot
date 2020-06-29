c2bf2c14a402be58c8c3362641f2452de5f34fc8
Gowtham0499 <gowth0499@gmail.com>
2020-06-29 13:18:37 +0530

    Added Test Class

--- studentdal/src/test/java/com/fullstack/student/dal/StudentdalApplicationTests.java
+++ studentdal/src/test/java/com/fullstack/student/dal/StudentdalApplicationTests.java
@@ -1,13 +1,25 @@
 package com.fullstack.student.dal;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fullstack.student.dal.entities.Student;
+import com.fullstack.student.dal.repositories.StudentRepository;
 
 @SpringBootTest
 class StudentdalApplicationTests {
 
+	@Autowired
+	private StudentRepository studentRepository;
+
 	@Test
-	void contextLoads() {
+	public void testCreateStudent() {
+		Student student = new Student();
+		student.setName("John");
+		student.setCourse("Java Web Services");
+		student.setFee(800d);
+		studentRepository.save(student);
 	}
 
 }
