8e955df269c494840ca230817fb0e1046ef5c0cb
Gowtham0499 <gowth0499@gmail.com>
2020-07-03 11:33:24 +0530

    Created Model and Rest Client Classes

---
+++ flightcheckin/src/main/java/com/flight/checkin/integration/ReservationRestClientImpl.java
@@ -0,0 +1,20 @@
+package com.flight.checkin.integration;
+
+import com.flight.checkin.dto.Reservation;
+import com.flight.checkin.dto.ReservationUpdateRequest;
+
+public class ReservationRestClientImpl implements ReservationRestClient {
+
+	@Override
+	public Reservation findReservation(Long id) {
+		
+		return null;
+	}
+
+	@Override
+	public Reservation updateReservation(ReservationUpdateRequest request) {
+		
+		return null;
+	}
+
+}
