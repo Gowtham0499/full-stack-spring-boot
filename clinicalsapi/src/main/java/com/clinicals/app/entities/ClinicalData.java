ae3b77503302777ea607d8c9096f1a13e59e47d7
Gowtham0499 <gowth0499@gmail.com>
2020-07-15 12:08:09 +0530

    Fixes

--- clinicalsapi/src/main/java/com/clinicals/app/entities/ClinicalData.java
+++ clinicalsapi/src/main/java/com/clinicals/app/entities/ClinicalData.java
@@ -57,4 +57,12 @@ public class ClinicalData {
 		this.measuredDateTime = measuredDateTime;
 	}
 
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
 }
