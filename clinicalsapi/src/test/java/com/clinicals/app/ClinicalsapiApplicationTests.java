e01381eda6660346cd7321d91c03d17a2c55442c
Gowtham0499 <gowth0499@gmail.com>
2020-07-15 11:30:40 +0530

    Initialized with Entities

---
+++ clinicalsapi/src/test/java/com/clinicals/app/ClinicalsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicals.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
