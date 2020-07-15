9548661d214dbb0d3f1cf70dec1edc8139c10d11
Gowtham0499 <gowth0499@gmail.com>
2020-07-15 13:54:09 +0530

    Added Repository Finder Methods

--- clinicalsapi/src/main/java/com/clinicals/app/repositories/ClinicalDataRepository.java
+++ clinicalsapi/src/main/java/com/clinicals/app/repositories/ClinicalDataRepository.java
@@ -1,9 +1,13 @@
 package com.clinicals.app.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.clinicals.app.entities.ClinicalData;
 
 public interface ClinicalDataRepository extends JpaRepository<ClinicalData, Integer> {
+	
+	List<ClinicalData> findByPatientIdAndComponentNameOrderByMeasuredDateTime(int patientId, String componentName);
 
 }
